191669dc1d76c5b79174df589ff6f67c9e8b6c86
Cameron Larsen <camjlarsen@gmail.com>
2020-10-19 18:06:17 -0700

    Add Lab4

---
+++ CPPTemplate.cpp
@@ -0,0 +1,22 @@
+/*	
+	Program:
+	Author:
+	Course:
+	Date:
+	Description:
+
+	I certify that the following code is my own work.
+
+	Exception(s): N/A
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Author: First Name Last Name\n";
+
+	return 0;
+}
